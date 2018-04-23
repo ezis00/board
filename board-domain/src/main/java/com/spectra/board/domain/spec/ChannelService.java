8ecd5fa8e2a6f32629fd624e5b469c9a4d9e8cc1
leejh <ezis@hanmail.net>
2018-04-23 13:06:41 +0900

    create board-store-map

--- board-domain/src/main/java/com/spectra/board/domain/spec/ChannelService.java
+++ board-domain/src/main/java/com/spectra/board/domain/spec/ChannelService.java
@@ -1,5 +1,15 @@
 package com.spectra.board.domain.spec;
 
+import com.spectra.board.domain.entity.Channel;
+import com.spectra.board.domain.granule.NameValueList;
+
 public interface ChannelService
 {
+    String regist(Channel channelCdo);
+
+    Channel find(String channelId);
+
+    Channel findByTitle(String title);
+
+    void modify(String channelId, NameValueList nameValueList);
 }
